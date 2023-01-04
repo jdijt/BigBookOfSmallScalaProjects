c976e7e54ac42ff30a4a25e876c296ea58e7741a
Jasper Dijt <jasper@jasperdijt.eu>
2023-01-04 11:50:18 +0100

    Better encapsulation

--- src/main/scala/eu/derfniw/project01Bagels/Bagels.scala
+++ src/main/scala/eu/derfniw/project01Bagels/Bagels.scala
@@ -23,9 +23,9 @@ val success    = "You got it!"
 val thanks     = "Thanks for playing!"
 val notANumber = "This is not a number, try again!"
 
-def guess(in: Int)                   = s"Guess #$in"
-def invalidInput(in: String): String = s"I do not understand $in. Please say yes or no."
-def outOfGuesses(in: String): String = s"You're out of guesses, the number was $in."
+def guessMsg(in: Int)                   = s"Guess #$in"
+def invalidInputMsg(in: String): String = s"I do not understand $in. Please say yes or no."
+def outOfGuessesMsg(in: String): String = s"You're out of guesses, the number was $in."
 
 def compareValues(guess: String, secret: String): String =
   val clues = guess.zip(secret).collect {
@@ -34,52 +34,54 @@ def compareValues(guess: String, secret: String): String =
   }
   if clues.length == 0 then "Bagels" else clues.mkString(" ")
 
-/**
-  * Handles a guess.
-  * Must know how to complete a game when we run out of guesses or when the guess is correct.
-  */
-def handleGuess(currentGuess: Int, secretNumber: String, completer: IO[Unit]): IO[Unit] =
-  if currentGuess > guesses then Console[IO].println(outOfGuesses(secretNumber)) *> completer
-  else
-    for
-      _     <- Console[IO].println(guess(currentGuess))
-      input <- Console[IO].readLine
-      _ <-
-        if input == secretNumber then Console[IO].println(success) *> completer
-        else if input.forall(_.isDigit) then
-          Console[IO].println(compareValues(input, secretNumber)) *> handleGuess(
-            currentGuess + 1,
-            secretNumber,
-            completer
-          )
-        else Console[IO].println(notANumber) *> handleGuess(currentGuess, secretNumber, completer)
-    yield ()
+class Bagels(generator: Random[IO], secret: Ref[IO, String]):
+  /** Generates a secret with the supplied generator & kicks off the guessing.
+    */
+  def start: IO[Unit] = for
+    newSecret <- generator.betweenInt(100, 1000).map(_.toString)
+    _         <- secret.set(newSecret)
+    _         <- Console[IO].println(numberPickedText)
+    _         <- gameLoop(1)
+  yield ()
 
-/**
-  * Completes the game and asks for a restart, must know how to start a game.
-  */
-def gameCompleted(starter: IO[Unit]): IO[Unit] = for
-  _        <- Console[IO].println(playAgain)
-  response <- Console[IO].readLine
-  _ <- response match
-         case "no"  => Console[IO].println(thanks) *> IO.unit
-         case "yes" => starter
-         case _ =>
-           Console[IO].println(invalidInput(response)) *> gameCompleted(starter)
-yield ()
+  /** Completes the game and asks for a restart, must know how to start a game.
+    */
+  def complete: IO[Unit] = for
+    _        <- Console[IO].println(playAgain)
+    response <- Console[IO].readLine
+    _ <- response match
+           case "no"  => Console[IO].println(thanks) *> IO.unit
+           case "yes" => start
+           case _ =>
+             Console[IO].println(invalidInputMsg(response)) *> complete
+  yield ()
 
-/**
-  * Generates a secret with the supplied generator & kicks off the guessing.
-  */
-def startGame(rand: Random[IO]): IO[Unit] = for
-  secret <- rand.betweenInt(100, 1000).map(_.toString)
-  _      <- Console[IO].println(numberPickedText)
-  _      <- handleGuess(1, secret, gameCompleted(startGame(rand)))
-yield ()
+  def gameLoop(currentGuess: Int): IO[Unit] = for
+    secretNumber <- secret.get
+    _ <- if currentGuess > guesses then outOfGuesses(secretNumber)
+         else handleGuess(currentGuess, secretNumber)
+  yield ()
+
+  def outOfGuesses(secretNum: String): IO[Unit] =
+    Console[IO].println(outOfGuessesMsg(secretNum)) *> complete
+
+  def handleGuess(currentGuess: Int, secretNumber: String): IO[Unit] = for
+    _     <- Console[IO].println(guessMsg(currentGuess))
+    input <- Console[IO].readLine
+    _ <-
+      if input == secretNumber then Console[IO].println(success) *> complete
+      else if input.forall(_.isDigit) then
+        Console[IO].println(compareValues(input, secretNumber)) *> gameLoop(
+          currentGuess + 1
+        )
+      else Console[IO].println(notANumber) *> handleGuess(currentGuess, secretNumber)
+  yield ()
+end Bagels
 
 object Bagels extends IOApp.Simple:
   def run: IO[Unit] = for
     _         <- Console[IO].println(welcomeText)
     generator <- Random.scalaUtilRandom[IO]
-    _         <- startGame(generator)
+    secretRef <- Ref[IO].of("000")
+    _         <- Bagels(generator, secretRef).start
   yield ()
